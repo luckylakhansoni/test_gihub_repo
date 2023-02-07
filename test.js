12057dde89c0ffc7d7bb835258f5a4c68864b291
Lakhan soni <lakhan.soni@neosoftmail.com>
2023-02-07 18:46:46 +0530

    some changes

--- test.js
+++ test.js
@@ -2,3 +2,8 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
