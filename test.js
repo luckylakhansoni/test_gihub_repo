026db9cfb731753730a515454c139048d42eadb4
= <=>
2023-02-09 13:09:02 +0530

    some changes

--- test.js
+++ test.js
@@ -7,3 +7,8 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
